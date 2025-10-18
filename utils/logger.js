ce5447abf02617036636cff9fdc9a1054e4fcc2d
Hosung Kang <karosung@gmail.com>
2025-10-18 12:00:57 +0900

    [GPT] Format logger timestamps with KST label

--- utils/logger.js
+++ utils/logger.js
@@ -4,15 +4,28 @@ const { createLogger, format, transports } = require("winston");
 
 const level = process.env.LOG_LEVEL || "info";
 
+const kstFormatter = new Intl.DateTimeFormat("ko-KR", {
+  timeZone: "Asia/Seoul",
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  hour12: false,
+});
+
 const consoleFormat = format.combine(
   format.colorize({ all: true }),
-  format.timestamp(),
+  format.timestamp({
+    format: () => kstFormatter.format(new Date()),
+  }),
   format.errors({ stack: true }),
   format.splat(),
   format.printf(({ timestamp, level: lvl, message, ...meta }) => {
     const metaString =
       Object.keys(meta).length > 0 ? ` ${JSON.stringify(meta)}` : "";
-    return `${timestamp} [${lvl}] ${message}${metaString}`;
+    return `[${timestamp} KST] [${lvl}] ${message}${metaString}`;
   })
 );
 
